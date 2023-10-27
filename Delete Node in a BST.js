62f59f493dce59cdb7a67d94d95f27a0e75ddbf3
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-10-27 18:32:14 +0530

    Delete Delete Node in a BST.js

--- Delete Node in a BST.js
+++
@@ -1,3 +0,0 @@
-var deleteNode = function(root, key) {
-    return !root ? null : (root.val === key ? (root.left ? (root.right ? (root.right.left = root.left) : root.left) : root.right) : (key > root.val ? (root.right = deleteNode(root.right, key)) : (root.left = deleteNode(root.left, key))), root);
-};
